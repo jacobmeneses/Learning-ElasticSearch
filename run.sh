06be649264edc44a9c92f7aaaae40f0932d29af8
Jacob Meneses <jaykobyou@gmail.com>
2020-09-06 19:45:01 -0500

    Updating image version used

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name learning_elastic -d docker.elastic.co/elasticsearch/elasticsearch:7.0.1
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name learning_elastic -d docker.elastic.co/elasticsearch/elasticsearch:7.9.1
 
 
